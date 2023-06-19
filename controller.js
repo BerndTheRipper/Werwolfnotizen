813b2bbfae8f841c7587c5382b68f86494039d3e
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2023-06-19 22:48:56 +0200

    added extra security to frontend and controller

--- controller.js
+++ controller.js
@@ -3,6 +3,12 @@ class Controller {
     view;
     
     constructor(model, view){
+        if(!model instanceof Model){
+            throw new TypeError("model parameter must be an instance of Model.");
+        }
+        if(!view instanceof View){
+            throw new TypeError("view parameter must be an instance of View.");
+        }
         this.model = model;
         this.view = view;
     }
--- frontend.js
+++ frontend.js
@@ -2,6 +2,9 @@ class Frontend {
     model;
     
     constructor(model){
+        if(!model instanceof Model){
+            throw new TypeError("model parameter must be an instance of Model.");
+        }
         this.model = model;
     }
 }
