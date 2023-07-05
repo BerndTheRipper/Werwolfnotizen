6dfd9adaf74e0bcc78d8405b5f1e82d72f343a26
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2023-07-05 17:48:52 +0200

    done first adaptations of test to become my thing

--- index.test.js
+++ index.test.js
@@ -1,17 +1,19 @@
 const timeout = 5000;
 
-describe(
-    '/ (Home Page)',
-    () => {
-        let page;
-        beforeAll(async () => {
-            page = await globalThis.__BROWSER_GLOBAL__.newPage();
-            await page.goto('https://google.com');
-        }, timeout);
+var page;
+beforeAll(async () => {
+    page = await globalThis.__BROWSER_GLOBAL__.newPage();
+    await page.goto('http://localhost:8000');
+}, timeout);
 
+describe(
+    'Initial View loading properly',
+    () => {
         it('should load without error', async () => {
             const text = await page.evaluate(() => document.body.textContent);
-            expect(text).toContain('google');
+            const model = await page.evaluate(()=> model)
+            expect(text).toContain('Notiz an mich:');
+            expect(typeof model).toBe("object");
         });
     },
     timeout,
