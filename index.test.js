f088c9090d67376e4d5677a4d0556faa750ed531
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2023-07-06 17:23:09 +0200

    further experimenting with jest TODO figure out how to turn on intellisense

--- index.test.js
+++ index.test.js
@@ -11,9 +11,21 @@ describe(
     () => {
         it('should load without error', async () => {
             const text = await page.evaluate(() => document.body.textContent);
-            const model = await page.evaluate(()=> model)
-            expect(text).toContain('Notiz an mich:');
-            expect(typeof model).toBe("object");
+            const inputElement = await page.waitForSelector("#view > form > input[type=text]:nth-child(1)");
+            const testResults = await page.evaluate(() => {
+                let output = [];
+                let view = document.querySelector("#view");
+                output.push(view.querySelector("input").autocomplete == "off")
+                return output;
+            });
+            const model = await page.evaluate(()=> model);
+            // page.click(inputElement, {clickCount: 1});
+            
+            console.log(inputElement.placeholder);
+            expect(typeof text).not.toBe('function');
+            for(var i = 0; i < testResults.length; i++){
+                expect(testResults[i]).not.toBe(false);
+            }
         });
     },
     timeout,
