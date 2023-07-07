d8f6fdc1fac2f89ea566cc5ddd56108d964a08c4
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2023-07-07 23:04:02 +0200

    figuring out intellisense in globalThis This is really just a commit so I have one commit a day, but I've been working really hard on getting IntelliSense to work on globalThis.__BROWSER_GLOBAL__ or finding a viable alternative to it

--- index.test.js
+++ index.test.js
@@ -1,27 +1,37 @@
+const puppeteer = require('puppeteer');
+var setup = require("./setup");
+
+// var browser = await puppeteer.launch({headless:"new"});
+// var browser = globalThis.__BROWSER_GLOBAL__;
+// var browser = global.__BROWSER__;
+var browser = setup.browser;
+
 const timeout = 5000;
 
 var page;
 beforeAll(async () => {
-    page = await globalThis.__BROWSER_GLOBAL__.newPage();
+    page = await browser.newPage();
     await page.goto('http://localhost:8000');
 }, timeout);
 
 describe(
     'Initial View loading properly',
     () => {
+        
         it('should load without error', async () => {
             const text = await page.evaluate(() => document.body.textContent);
             const inputElement = await page.waitForSelector("#view > form > input[type=text]:nth-child(1)");
             const testResults = await page.evaluate(() => {
                 let output = [];
+                
                 let view = document.querySelector("#view");
-                output.push(view.querySelector("input").autocomplete == "off")
+                output.push(view.querySelector("input").autocomplete == "off");
+                
                 return output;
             });
             const model = await page.evaluate(()=> model);
             // page.click(inputElement, {clickCount: 1});
-            
-            console.log(inputElement.placeholder);
+            // console.log(inputElement.placeholder);
             expect(typeof text).not.toBe('function');
             for(var i = 0; i < testResults.length; i++){
                 expect(testResults[i]).not.toBe(false);
