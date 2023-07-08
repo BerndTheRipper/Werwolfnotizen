9606f6df627764c7dac20835ad201e62a655dfa4
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2023-07-08 19:00:34 +0200

    further experimenting still just commiting for one commit a day but really been working hard on this one

--- index.test.js
+++ index.test.js
@@ -1,14 +1,14 @@
 const puppeteer = require('puppeteer');
-var setup = require("./setup");
 
-// var browser = await puppeteer.launch({headless:"new"});
-// var browser = globalThis.__BROWSER_GLOBAL__;
-// var browser = global.__BROWSER__;
-var browser = setup.browser;
 
 const timeout = 5000;
 
-var page;
+//This code exists merely so I can have intellisense while refering to globalThis.__BROWSER_GLOBAL__
+//It makes the types be set to puppeteer.Browser and puppeteer.Page
+// var browser = await puppeteer.launch({headless:"new"});
+// var page = await browser.newPage();
+var browser = globalThis.__BROWSER_GLOBAL__;
+
 beforeAll(async () => {
     page = await browser.newPage();
     await page.goto('http://localhost:8000');
@@ -19,17 +19,25 @@ describe(
     () => {
         
         it('should load without error', async () => {
+            
             const text = await page.evaluate(() => document.body.textContent);
-            const inputElement = await page.waitForSelector("#view > form > input[type=text]:nth-child(1)");
+            const roleInputElement = await page.waitForSelector("#view > form > input[type=text]:nth-child(1)");
+            const amountInputElement = await page.waitForSelector("#view > form > input[type=number]:nth-child(2)");
             const testResults = await page.evaluate(() => {
                 let output = [];
                 
                 let view = document.querySelector("#view");
                 output.push(view.querySelector("input").autocomplete == "off");
-                
+
                 return output;
             });
             const model = await page.evaluate(()=> model);
+            page.click(roleInputElement);
+            page.type(roleInputElement, "Barde");
+
+            page.click(amountInputElement);
+            page.type(amountInputElement, "2");
+            // page.keyboard.press("Enter");
             // page.click(inputElement, {clickCount: 1});
             // console.log(inputElement.placeholder);
             expect(typeof text).not.toBe('function');
