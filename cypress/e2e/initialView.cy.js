164b7c0f655a8a79f1243a526ea990f3686a5278
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2023-09-16 23:58:59 +0200

    got some initial view testing going

--- cypress/e2e/initialView.cy.js
+++ cypress/e2e/initialView.cy.js
@@ -1,8 +1,22 @@
 // <reference types="Cypress" />
 
 describe('template spec', () => {
-	it('Tests if everything is there', () => {
+	it('Doesnt submit when typing unknown role with no number', () => {
+		var roleToTest = "Eine Rolle, die ich nicht kenne";
+
 		cy.visit("http://localhost:8000");
 
+		var role = cy.get("#view").find("input[name=roleName]");
+		role.type(roleToTest);
+
+		var amount = cy.get("#view").find("input[name=roleAmount]");
+		// amount.type("20");
+
+		var stub = cy.stub();
+		cy.on("window:alert", stub);
+
+		cy.get("#view").find("input[type=submit]").click();
+
+		expect(stub).to.not.called;
 	});
 });
