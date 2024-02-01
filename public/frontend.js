9a8796fa85cc9f0d87d10cd7a39fa24d6188076a
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2024-02-01 23:41:32 +0100

    I think I just made hunterTargetsToday obsolete

--- public/frontend.js
+++ public/frontend.js
@@ -772,16 +772,6 @@ class DayView extends View {
 	 * @param {Element} hunterTargetSection The section in which the hunter target section should be placed
 	 */
 	redrawHunterTargetSection(hunterTargetSection) {
-		if (this.model.hunterTargetsToday == 0) {
-			hunterTargetSection.innerHTML = "";
-			return;
-		}
-
-		//No check if hunterTargetsToday == 0 because if it is, the above check would have ended the function
-		// if (hunterTargetSection.innerHTML == "") {
-		hunterTargetSection.innerHTML = document.querySelector(".hunterTargetSection").innerHTML;
-		// }
-
 		let tbody = hunterTargetSection.querySelector("tbody");
 
 		let dyingHunters = [];
@@ -799,6 +789,12 @@ class DayView extends View {
 				killingHunters[killer.playerName] = proposal.player.playerName;
 			}
 		}
+
+		if (dyingHunters.length == 0) {
+			hunterTargetSection.innerHTML = "";
+			return;
+		}
+		hunterTargetSection.innerHTML = document.querySelector(".hunterTargetSection").innerHTML;
 
 		for (let hunter of dyingHunters) {
 			let tr = this._generateTableRows(2);
